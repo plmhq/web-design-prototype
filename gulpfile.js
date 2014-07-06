54cfd76516f5301de5baba50cf9f46cef55ce6be
srph <seraphipod@gmail.com>
2014-07-06 20:55:21 +0800

    Added (npm) package.json. Updated gulpfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,9 @@
 var assets = './assets/',
 	sheets = './sass/';
 
-var gulp 	= require('gulp'),
-	sass 	= require('gulp-ruby-sass'),
-	minify	= require('gulp-minify-css');
+var gulp 		= require('gulp'),
+	sass 		= require('gulp-ruby-sass'),
+	minify		= require('gulp-minify-css'),
+	livereload	= require('gulp-livereload'),
+	rename		= require('gulp-rename'),
+	notify		= require('gulp-notify');
