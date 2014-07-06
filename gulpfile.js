2ae1ec18ea743f91669f39523f032ac94e68468b
srph <seraphipod@gmail.com>
2014-07-06 21:15:36 +0800

    Updated gulpfile. Updated gulp dependencies (+updated + added express) ;

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,57 @@
-var assets = './assets/',
-	sheets = './sass/';
+// Directories
+var _assets = './assets/',
+	_sheets = './sass/';
 
+// Gulp dependencies
 var gulp 		= require('gulp'),
+	autoprefixer= require('gulp-autoprefixer'),
 	sass 		= require('gulp-ruby-sass'),
 	minify		= require('gulp-minify-css'),
 	livereload	= require('gulp-livereload'),
 	rename		= require('gulp-rename'),
-	notify		= require('gulp-notify');
+	notify		= require('gulp-notify'),
+	install 	= require('gulp-install');
+
+// Server
+var express 	= require('express'),
+	app 		= express();
+
+// Installs all bower components
+gulp.task('install', function() {
+	gulp.src(['./bower.json'])
+		.pipe(install())
+		.pipe(notify({ message: 'Bower components installed!' }));
+});
+
+// Run SaSS tasks
+gulp.task('sass', function() {
+	gulp.src(_sheets + '**/*.scss')
+		.pipe(sass({ style: 'expanded' }))
+		.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 11', 'ios 6', 'android 4'))
+		.pipe(rename({ suffix: '.min' }))
+		.pipe(minify())
+		.pipe(gulp.dest(_assets + 'css'))
+		.pipe(livereload())
+		.pipe(notify({ message: 'SaSS tasks finished!' }));
+});
+
+// Static server with express
+gulp.task('server', function() {
+	app.use(express.static(__dirname));
+
+	var server = app.listen(8080, function() {
+		console.log('Listening on port %d', server.address().port);
+	});
+})
+
+// Default task
+gulp.task('default', function() {
+	gulp.run('install');
+	gulp.run('sass');
+
+	gulp.watch(sass + '**/*.scss', ['sass']);
+	gulp.watch('./index.html')
+		.on('change', function(file) {
+			livereload().changed(file.path);
+		});
+})
